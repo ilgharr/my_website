43ae98178d24cf9f8242c5acb3153e91b1e3c8a9
ilgharr <ilgharrahno2.0@gmail.com>
2024-07-25 22:43:31 -0400

    replace Container element with Nav.Link, no alignment issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,42 @@ import React from 'react';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import kitty from '../images/orange_cat.png';
 import AboutMe from './AboutMe';
 import ContactMe from './ContactMe';
 
 export default function Navigation() {
+
+    const textWhite = "text-white  mx-auto"
+
     return (
+        <Navbar bg="light" expand="lg">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+            <img src={kitty} className="kitty" alt="logo orange cat image"/>
+                <Nav className="mr-auto w-100">
 
-            <Navbar className="navbar">
-                <link rel="stylesheet"
-                      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-                <img src={kitty} className="kitty" alt="logo orange cat image"/>
-
-                <div className="github-container">
-                    <Nav.Link href="https://github.com/ilgharr">
+                    <Nav.Link className={textWhite} href="https://github.com/ilgharr" target="_blank">
                         <i className="fa fa-github"></i>
-                        GitHub</Nav.Link>
-                </div>
+                        GitHub
+                    </Nav.Link>
 
-                <div className="linkedin-container">
-                    <Nav.Link href="https://www.linkedin.com/in/ilghar-rahno-5a40aa220/">
-                        <i className="fa fa-linkedin-square">
-                        </i>Linkedin</Nav.Link>
-                </div>
+                    <Nav.Link className={textWhite} href="https://www.linkedin.com/in/ilghar-rahno-5a40aa220/" target="_blank">
+                        <i className="fa fa-linkedin-square"></i>
+                        Linkedin
+                    </Nav.Link>
 
-                <div className="about-me-container">
-                    <AboutMe/>
-                </div>
+                    <Nav.Link className={textWhite}>
+                        <AboutMe/>
+                    </Nav.Link>
 
-                <div className="contact-me-container">
-                    <ContactMe/>
-                </div>
-            </Navbar>
+                    <Nav.Link className={textWhite}>
+                        <ContactMe/>
+                    </Nav.Link>
 
-
+                </Nav>
+        </Navbar>
 
     )
 }
