2848a5de372d3196f3abb158dcde097a2ec8201a
ilgharr <ilgharrahno2.0@gmail.com>
2024-08-08 00:18:57 -0400

    'home page coming soon' added and centered the navbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AboutMe from './AboutMe';
-import ContactMe from './ContactMe';
-import {Container} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 
 export default function Main() {
     return (
-        <Container>
-            MAIN PAGE!
+        <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh', width: '90vw', backgroundColor: '#000', color: '#fff' }}>
+            <Row className="text-center">
+                <Col>
+                    <h1 style={{ fontSize: '5rem' }}>Home Page Coming Soon</h1>
+                    <p style={{ fontSize: '1.5rem' }}>We are working hard to get this site up and running.</p>
+                </Col>
+            </Row>
         </Container>
     )
 }
